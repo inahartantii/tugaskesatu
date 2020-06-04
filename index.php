28f65aee37ce2681568fca99013d72cf857ed1da
inahartantii <inahartanti123@gmail.com>
2020-06-04 13:07:02 +0700

    project kesatu

---
+++ index.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+    <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
+      <div class="container"></div>
+        <a class="navbar-brand" href="#">Ina Hartanti</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse"
+         data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup"
+         aria-expanded="false" aria-label="Toggle navigation">
+         <span class="navbar-toggle-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div class="navbar-nav">
+            <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+            <a class="nav-item nav-link" href="#">About</a>
+            <a class="nav-item nav-link" href="#">Portofolio</a>
+            <a class="nav-item nav-link disabled" href="#">Contact</a>
+        </div>
+      </div>
+    </nav>
+
+    <div class="jumbotrom jumbrotom-fluid"></div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="js/jquery-3.3.1.slim.min.js" ></script>
+    <script src="js/popper.min.js" ></script>
+    <script src="js/bootstrap.min.js" ></script>
+  </body>
+</html>
